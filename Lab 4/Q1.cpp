f038d6c2392912a7bf5afc021e9059e1f3f3389a
Usman Hasan <usmanhasan197@gmail.com>
2025-02-10 08:35:27 +0500

    Create Q1.cpp

---
+++ Lab 4/Q1.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+
+class student{
+    int studentid;
+    string name;
+    int age;
+    char grade;
+    public:
+   student(){
+       grade='N';
+   }
+    student(int i,string n,int a,char g){
+       studentid=i;
+       name=n;
+       age=a;
+       grade=g;
+   }
+   
+   void promoteStudent(){
+       --grade;
+   }
+   string isEligibleForScholarship(){
+       string res="False";
+       if ( grade =='A'){
+           res="True";
+           return res;
+       }
+       return res;
+   }
+   void displayDetails(){
+       cout<<"\n";
+       cout<<"Student ID: "<<studentid<<endl;
+       cout<<"Name: "<<name<<endl;
+       cout<<"Age: "<<age<<endl;
+       cout<<"Grade: "<<grade<<endl;
+   }
+   
+    
+};
+
+int main() {
+   string el;
+   student s1;
+   student s2(22,"John",17,'C');
+   s2.promoteStudent();
+   s2.promoteStudent();
+   
+   el=s2.isEligibleForScholarship();
+   
+   cout<<"student 2: "<<endl;
+   s2.displayDetails();
+   cout<<"Student 2 is Eligible For Scholarship: "<<el<<endl;
+   cout<<"\n";
+   cout<<"student 1: "<<endl;
+   s1.displayDetails();
+
+    return 0;
+}
